76f6256fcb47588f5df00ecd6a0b99056ed9b91d
Carleigh Crockett <capleugh@gmail.com>
2020-02-06 17:10:40 -0700

    Add spec for user repos on user dashboard
    Co-authored-by: Jomah Fangonilo <53122061+jfangonilo@users.noreply.github.com>

---
+++ spec/features/user/user_dashboard_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+feature "As a logged in user" do
+  feature "when I visit my dashboard" do
+    scenario "I see a section for github and see five repos which link to the repo on github", :vcr do
+      user = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/dashboard'
+
+      expect(page).to have_content("Github")
+      expect(page).to have_link("battleship")
+      expect(page).to have_link("activerecord-obstacle-course")
+      expect(page).to have_link("adopt_dont_shop")
+      expect(page).to have_link("adopt_dont_shop_paired")
+      expect(page).to have_link("apartments_1908")
+    end
+  end
+end
