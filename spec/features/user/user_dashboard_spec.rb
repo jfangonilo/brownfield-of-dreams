e64d227dff7666e97b22f4508a36bda2119222f7
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-07 14:11:18 -0700

    Add user token sad path spec

--- spec/features/user/user_dashboard_spec.rb
+++ spec/features/user/user_dashboard_spec.rb
@@ -4,20 +4,25 @@ feature "As a logged in user" do
   feature "when I visit my dashboard" do
     scenario "I see a section for github and see five repos which link to the repo on github", :vcr do
       user = create(:user, github_token: ENV['GITHUB_TOKEN'])
-      user_2 = create(:user, github_token: ENV['GITHUB_TOKEN_2'])
+      user_2 = create(:user)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit '/dashboard'
 
       expect(page).to have_content("Github")
-      expect(page).to have_link("battleship")
-      expect(page).to have_link("activerecord-obstacle-course")
-      expect(page).to have_link("adopt_dont_shop")
-      expect(page).to have_link("adopt_dont_shop_paired")
-      expect(page).to have_link("apartments_1908")
+      expect(page).to have_css('.repositories', count: 5)
+      within(first('.repositories')) do
+        expect(page).to have_link('activerecord-obstacle-course')
+      end
+    end
 
-      expect(page).to_not have_link("backend_module_0_capstone")
+    scenario "if user does not have token, 'Github' and repos do not display" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit '/dashboard'
+      expect(page).to_not have_content("Github")
+      expect(page).not_to have_css('.repositories')
     end
   end
 end
