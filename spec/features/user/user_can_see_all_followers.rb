fab478f54a157f3d1bac90f5d91dae46332e9522
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-08 13:35:33 -0700

    Add sad path test to followers
    Co-authored-by: Carleigh Crockett <47065120+Capleugh@users.noreply.github.com>

--- spec/features/user/user_can_see_all_followers.rb
+++ spec/features/user/user_can_see_all_followers.rb
@@ -10,4 +10,15 @@ feature 'As a logged in user, when I visit my dashboard' do
 
     expect(page).to have_css('.followers')
   end
+
+  scenario "I can't see my followers if I'm not authenticated" do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/dashboard'
+
+    expect(page).to_not have_content("Followers")
+    expect(page).not_to have_css('.followers')
+  end
 end
