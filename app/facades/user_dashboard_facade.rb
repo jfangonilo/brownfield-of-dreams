df1c869acabb2debb1b539b517d81cc578429d26
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-07 20:55:59 -0700

    Add name, email, and follower methods to facade
    Co-authored-by: Carleigh Crockett <47065120+Capleugh@users.noreply.github.com>

--- app/facades/user_dashboard_facade.rb
+++ app/facades/user_dashboard_facade.rb
@@ -3,9 +3,31 @@ class UserDashboardFacade
     @user = user
   end
 
+  def user_has_token?
+    @user.github_token
+  end
+
+  def first_name
+    @user.first_name
+  end
+
+  def last_name
+    @user.last_name
+  end
+
+  def email
+    @user.email
+  end
+
   def repositories
     service.repo_info.take(5).map do |data|
       Repository.new(data)
+    end
+  end
+
+  def followers
+    service.follower_info.map do |data|
+      Follower.new(data)
     end
   end
 
