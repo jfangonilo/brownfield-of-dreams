2890604b278125eb337ee842a0733fb38f2b9783
Carleigh Crockett <capleugh@gmail.com>
2020-02-12 16:59:14 -0700

    Add self joins relationships
    Co-authored-by: Jomah Fangonilo 
    <53122061+jfangonilo@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
   has_many :user_videos
   has_many :videos, through: :user_videos
 
+  has_many :friendships
+  has_many :friended_users, through: :friendships
+  
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: :friended_user_id
+  has_many :users_who_friended, through: :inverse_friendships, source: :user
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password_digest
   validates_presence_of :first_name
