e307db276eb865f3d7f10c3c0cd540897bb9ab00
Carleigh Crockett <capleugh@gmail.com>
2020-02-12 16:56:32 -0700

    Add test for has_acount method
    Co-authored-by: Jomah Fangonilo 
    <53122061+jfangonilo@users.noreply.github.com>

--- spec/models/github_user_spec.rb
+++ spec/models/github_user_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
 describe GithubUser do
-  it 'has attributes' do
-    attributes = {
+  before(:each) do
+    @attributes = {
       login: "jfangonilo",
       html_url: "https://github.com/jfangonilo",
       id: 56
     }
-
-    gh_user = GithubUser.new(attributes)
+  end
+  it 'has attributes' do
+    gh_user = GithubUser.new(@attributes)
     expect(gh_user.handle).to eq "jfangonilo"
     expect(gh_user.url).to eq "https://github.com/jfangonilo"
     expect(gh_user.github_id).to eq 56
+  end
+
+  it 'can find whether or not a user has an account by github_id' do
+    gh_user = GithubUser.new(@attributes)
+    db_user = create(:user, github_id: 56)
+
+    result = gh_user.has_account?
+
+    expect(result.github_id).to eq(db_user.github_id)
   end
 end
