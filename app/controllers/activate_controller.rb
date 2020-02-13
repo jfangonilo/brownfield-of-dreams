76cbfe2f2d68dc397824344c06a88355bc3ed895
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-13 08:28:13 -0700

    Remove update action
    Co-authored-by: Carleigh Crockett <47065120+Capleugh@users.noreply.github.com>

--- app/controllers/activate_controller.rb
+++ app/controllers/activate_controller.rb
@@ -1,10 +1,5 @@
 class ActivateController < ApplicationController
   def create
-    # require "pry"; binding.pry
     UserActivatorMailer.inform(current_user).deliver_now
-  end
-
-  def update
-    require "pry"; binding.pry
   end
 end
