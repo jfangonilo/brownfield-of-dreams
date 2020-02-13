cd1aebf2f92218ad2c4f7595097a3ecfe31105a5
Carleigh Crockett <capleugh@gmail.com>
2020-02-12 20:48:38 -0700

    Update controller to show success flash message
    Co-authored-by: Jomah Fangonilo 
    <53122061+jfangonilo@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
     user = User.create(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:success] = "Logged in as #{user.first_name} #{user.last_name}."
+
       redirect_to dashboard_path
     else
       flash[:error] = 'Username already exists'
