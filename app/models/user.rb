1d29d303f2fc0e30d336e31574d523fcadadb7b5
Carleigh Crockett <capleugh@gmail.com>
2020-02-12 19:16:12 -0700

    Add rubocop fixes
    Co-authored-by: Jomah Fangonilo 
    <53122061+jfangonilo@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
 
   has_many :friendships
   has_many :friended_users, through: :friendships
-  
-  has_many :inverse_friendships, class_name: "Friendship", foreign_key: :friended_user_id
+
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: :friended_user_id
   has_many :users_who_friended, through: :inverse_friendships, source: :user
 
   validates :email, uniqueness: true, presence: true
