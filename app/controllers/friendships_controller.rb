367f6e8e124b31a2767dc2eb34956c55469c8bc2
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-13 08:28:44 -0700

    Fix formatting
    Co-authored-by: Carleigh Crockett <47065120+Capleugh@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,8 @@
 class FriendshipsController < ApplicationController
   def create
-    friend = User.find_by(github_id: params[:friendship][:friend_github_id])
-
     user_id = params[:friendship][:user_id]
-
+    friend = User.find_by(github_id: params[:friendship][:friend_github_id])
     friendship = Friendship.new(user_id: user_id, friended_user_id: friend.id)
-
     if friendship.save
       flash[:success] = "#{friend.first_name} is now your friend!"
     else
