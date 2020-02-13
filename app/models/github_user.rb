293eccf4b9dd316bd5a97fa4a2a1cd32e4c76de1
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-13 08:30:28 -0700

    Add friend method
    Co-authored-by: Carleigh Crockett <47065120+Capleugh@users.noreply.github.com>

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -10,4 +10,12 @@ class GithubUser
   def account?
     User.find_by(github_id: github_id)
   end
+
+  def friend?
+    if Friendship.find_by(friended_user_id: account?.id)
+      true
+    else
+      false
+    end
+  end
 end
